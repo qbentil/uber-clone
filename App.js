6013c44f62402baefb2d4f4748d5a1f32fe1fe57
qbentil <Bentilshadrack72@gmail.com>
2022-02-08 13:21:27 +0000

    Init Test

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+
+import { StatusBar } from 'expo-status-bar';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Uber 2.0!</Text>
       <StatusBar style="auto" />
     </View>
   );
