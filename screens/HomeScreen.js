3913e907f03fbda49325fd2f5f8810896d3170fd
qbentil <Bentilshadrack72@gmail.com>
2022-02-09 14:03:15 +0000

    Creating Home Screen

---
+++ screens/HomeScreen.js
@@ -0,0 +1,22 @@
+import { StyleSheet, Text, View } from 'react-native'
+
+import React from 'react'
+
+const HomeScreen = () => {
+  return (
+    <View style = {container}>
+      <Text>HomeScreen!</Text>
+    </View>
+  )
+}
+
+export default HomeScreen
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+      },
+})
