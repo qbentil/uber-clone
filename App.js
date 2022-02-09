506b411fa7826dc4971751c33d37be8ae4cccec8
qbentil <Bentilshadrack72@gmail.com>
2022-02-09 13:29:10 +0000

    Implementing providers

--- App.js
+++ App.js
@@ -1,13 +1,18 @@
 import { StyleSheet, Text, View } from 'react-native';
 
+// SET UP REDUX
+import { Provider } from 'react-redux';
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Uber 2.0!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider>
+      <View style={styles.container}>
+        <Text>Uber 2.0!</Text>
+        <StatusBar style="auto" />
+      </View>
+    </Provider>
   );
 }
 
