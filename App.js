b6277f89f792cce145e30b7663727a3458a7fce4
qbentil <Bentilshadrack72@gmail.com>
2022-02-10 01:20:51 +0000

    Updated App
    Added SafeAreaProvider to App root

--- App.js
+++ App.js
@@ -1,16 +1,25 @@
 import { StyleSheet, Text, View } from 'react-native';
 
 import HomeScreen from './screens/HomeScreen';
-// SET UP REDUX
 import { Provider } from 'react-redux';
 import React from 'react';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { StatusBar } from 'expo-status-bar';
 import store from './store';
+
+// SET UP REDUX
+
+
+
+
+
 
 export default function App() {
   return (
     <Provider store={store}>
-      <HomeScreen />
+      <SafeAreaProvider>
+        <HomeScreen />
+      </SafeAreaProvider>
     </Provider>
   );
 }
