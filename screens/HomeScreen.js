36f0fb6baafe0375558f3bf30970dc19e7a72681
qbentil <Bentilshadrack72@gmail.com>
2022-02-10 01:22:01 +0000

    Designig HomePage
    Home Screen Design with Uber Logo

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,23 +1,31 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { Image, StyleSheet, Text, View } from 'react-native'
 
 import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import tw from 'twrnc';
 
 const HomeScreen = () => {
   return (
-    <View style = {styles.container}>
-      <Text>Welcome Home!</Text>
-      <Text>Vector Icons!</Text>
-    </View>
+    <SafeAreaView style = {tw`bg-white h-full`}>
+      <View style={tw`p-5`}>
+          {/* Adding Logo */}
+          <Image 
+            style = {styles.logo}
+            source={{
+                uri: "https://links.papareact.com/gzs" //uber logo link
+            }}
+          />
+      </View>
+    </SafeAreaView>
   )
 }
 
 export default HomeScreen
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-      },
+    logo: {
+        width: 100,
+        height: 100,
+        resizeMode: 'contain'
+    }
 })
