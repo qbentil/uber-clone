cf2201dee90973fd9424ace1d41066ef1c59b6a4
qbentil <Bentilshadrack72@gmail.com>
2022-02-11 09:17:45 +0000

    FInishing HomeScreen

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,11 +1,16 @@
-import { Image, StyleSheet, Text, View } from 'react-native'
+import { Image, Input, StyleSheet, Text, View } from 'react-native'
+import { setDestination, setOrigin } from '../slices/navSlice';
 
+import {GOOGLE_MAPS_APIKEY} from "@env"
+import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
 import NavOptions from '../components/NavOptions';
 import React from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import tw from 'twrnc';
+import { useDispatch } from 'react-redux';
 
 const HomeScreen = () => {
+    const dispatch = useDispatch()
   return (
     <SafeAreaView style = {tw`bg-white h-full`}>
       <View style={tw`p-5`}>
@@ -17,6 +22,42 @@ const HomeScreen = () => {
             }}
           />
       </View>
+      <GooglePlacesAutocomplete 
+        style={{
+            container: {
+                flex: 0,
+            },
+            textInput: {
+                fontSize: 18
+            }
+        }}
+        query={{
+            key: GOOGLE_MAPS_APIKEY,
+            language: 'en',
+            // components: 'country:ghana',
+        }}
+        // textInputProps={{
+        //     InputComp: Input,
+        //     leftIcon: { type: 'font-awesome', name: 'chevron-left' },
+        //     errorStyle: { color: 'red' },
+        // }}
+        onPress = {(data, detailes=null) => {
+            dispatch(setOrigin({
+                location: detailes.geometry.location,
+                description: data.description
+            }))
+
+            dispatch(setOrigin(null))
+        }}
+        
+        fetchDetails = {true}
+        nearbyPlacesAPI='GooglePlacesSearch'
+        debounce={400}
+        placeholder="Where from?"
+        enablePoweredByContainer = {false}
+        // currentLocation={true}
+        // currentLocationLabel='Current location'
+      />
       <NavOptions />
     </SafeAreaView>
   )
