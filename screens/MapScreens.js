fb16ac56199fcd18fddc59d01d97775b7664c4f1
qbentil <Bentilshadrack72@gmail.com>
2022-02-11 09:17:37 +0000

    Creating MapScreen

--- screens/MapScreens.js
+++ screens/MapScreens.js
@@ -1,17 +1,23 @@
 import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 
+import Map from '../components/Map';
 import React from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
+import tw from 'twrnc';
 import { useNavigation } from '@react-navigation/native'
 
 const MapScreens = () => {
     const navigation = useNavigation();
   return (
-    <SafeAreaView >
-      <TouchableOpacity onPress={() => navigation.navigate("HomeScreen")}>
-        <Text>Go Home</Text>
-      </TouchableOpacity>
-    </SafeAreaView>
+    <View>
+        <Text>This is thr map view</Text>
+        <View style={tw`h-1/2`}>
+            <Map />
+        </View>
+        <View style={tw`h-1/2`}>
+            
+        </View>
+    </View>
   )
 }
 
