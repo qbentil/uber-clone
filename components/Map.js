d26889433375282a2c48485cfbf4466097d887d1
qbentil <Bentilshadrack72@gmail.com>
2022-02-12 17:08:23 +0000

    Updated Map Component

--- components/Map.js
+++ components/Map.js
@@ -2,12 +2,16 @@ import MapView, {Marker} from 'react-native-maps';
 import { StyleSheet, Text, View } from 'react-native'
 
 import React from 'react'
+import {selectOrigin} from "../slices/navSlice"
 import tw from 'twrnc';
+import {useSelector} from "react-redux"
 
 const Map = () => {
+  const origin = useSelector(selectOrigin);
   return (
     <MapView
       style={tw`flex-1`}
+      mapType ="standard"
       initialRegion={{
         latitude: 5.646390,
         longitude: -0.181980,
