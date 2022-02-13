ff5d76e11f255201874a4f7dd34ea4b7a9fe8015
qbentil <Bentilshadrack72@gmail.com>
2022-02-13 07:14:09 +0000

    Created Navigation Cards for map Screen

---
+++ components/NavigateCard.js
@@ -0,0 +1,96 @@
+import {StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+import { setDestination, setOrigin } from '../slices/navSlice';
+
+import {GOOGLE_MAPS_APIKEY} from "@env"
+import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
+import { Icon } from 'react-native-elements';
+import NavFavourites from './NavFavourites';
+import React from 'react';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import tw from 'twrnc';
+import { useDispatch } from 'react-redux';
+import {useNavigation} from "@react-navigation/native"
+
+const NavigateCard =() => {
+    
+    const navigation = useNavigation()
+    const dispatch = useDispatch();
+    return (
+        <SafeAreaView style={tw`bg-white flex-1`}>
+            <Text style={tw`text-center text-xl mt--9`}> Welcome Bentil! </Text>
+            <View style={tw`border-t border-gray-200 flex-shrink`}>
+
+                <GooglePlacesAutocomplete 
+                    styles={inputBoxStyle}
+                    query={{
+                        key: GOOGLE_MAPS_APIKEY,
+                        language: 'en',
+                        components: 'country:gh',
+                    }}
+                    onPress = {(data, details=null) => {
+                        dispatch(setDestination({
+                            location: details.geometry.location,
+                            description: data.description
+                        }))
+
+                        // Navigate user to RideOption pagge
+                        navigation.navigate("RideOptionCard")
+                    }}
+                    minLength={2}
+                    returnKeyType= {"search"}
+                    fetchDetails = {true}
+                    nearbyPlacesAPI='GooglePlacesSearch'
+                    debounce={400}
+                    placeholder="Where to ?"
+                    enablePoweredByContainer = {false}
+
+                />
+            </View>
+            {/* <NavFavourites /> */}
+            <View  style={tw`flex-row bg-white justify-evenly py-2 mt-auto border-t border-gray-100`} >
+                <TouchableOpacity style={tw`flex flex-row bg-black w-24 px-4 justify-between py-3 rounded-full`}
+                    onPress={() => navigation.navigate('RideOptionCard')}
+                >
+                    <Icon 
+                        type = 'font-awesome'
+                        name = 'car'
+                        color={'white'}
+                        size={16}
+                    />
+                    <Text style={tw`text-white text-center`}>Car</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={tw`flex flex-row bg-white border w-24 px-4 justify-between py-3 rounded-full`}>
+                    <Icon 
+                        type = 'ionicon'
+                        name = 'fast-food-outline'
+                        color={'black'}
+                        size={16}
+                    />
+                    <Text style={tw`text-black text-center`}>Eat</Text>
+                </TouchableOpacity>
+            </View>
+        </SafeAreaView>
+    );
+}
+
+
+const inputBoxStyle = StyleSheet.create({
+    container: {
+        borderColor: "#fff",
+        paddingTop: 20,
+        flex: 0,
+        
+    },
+    textInputContainer: {
+        paddingHorizontal: 10,
+        paddingBottom: 0
+    },
+    textInput: {
+        height: 38,
+        borderRadius: 0,
+        fontSize: 18,
+        height: 45
+    }
+});
+
+export default NavigateCard
---
+++ components/RideOptionsCard.js
@@ -0,0 +1,18 @@
+import { StyleSheet, Text, View } from 'react-native';
+
+import React from 'react';
+
+const RideOptionsCard =() => {
+return (
+    <View>
+        <Text> RideOptionsCard </Text>
+    </View>
+    );
+}
+
+
+const styles = StyleSheet.create({
+    
+});
+
+export default RideOptionsCard
