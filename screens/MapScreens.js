85a7dc8fbfbed00e7c2cb0cb6ee379fcc6e661a4
qbentil <Bentilshadrack72@gmail.com>
2022-02-13 07:11:21 +0000

    Designing Map Screen

--- screens/MapScreens.js
+++ screens/MapScreens.js
@@ -1,21 +1,27 @@
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+import { StyleSheet, Text, View } from 'react-native'
 
 import Map from '../components/Map';
+import NavigateCard from '../components/NavigateCard';
 import React from 'react'
-import { SafeAreaView } from 'react-native-safe-area-context'
+import RideOptionsCard from '../components/RideOptionsCard';
+import { createStackNavigator } from '@react-navigation/stack';
 import tw from 'twrnc';
 import { useNavigation } from '@react-navigation/native'
 
+// import { KeyboardAvoidingView } from 'react-native-web';
+
 const MapScreens = () => {
-    const navigation = useNavigation();
+  const Stack = createStackNavigator();
   return (
     <View>
-        {/* <Text>This is thr map view</Text> */}
         <View style={tw`h-1/2`}>
             <Map />
         </View>
         <View style={tw`h-1/2`}>
-            
+            <Stack.Navigator>
+              <Stack.Screen name="MavigateCard" component={NavigateCard} options = {{headerShown: false}}/>
+              <Stack.Screen name="RideOptionCard" component={RideOptionsCard} options = {{headerShown: false}}/>
+            </Stack.Navigator>
         </View>
     </View>
   )
