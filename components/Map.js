5b00e22e6c2f1df58d36808a25e99ef1da9095db
qbentil <Bentilshadrack72@gmail.com>
2022-02-14 05:08:34 +0000

    Updated Maps Component with Derection Matrix API

--- components/Map.js
+++ components/Map.js
@@ -1,13 +1,13 @@
 import MapView, {Marker} from 'react-native-maps';
 import React ,{useEffect} from 'react'
 import { StyleSheet, Text, View } from 'react-native'
-import {selectDestination, selectOrigin} from "../slices/navSlice"
+import {selectDestination, selectOrigin, setTravelTimeInformation} from "../slices/navSlice"
 
 import {GOOGLE_MAPS_APIKEY} from "@env"
 import MapViewDirections from 'react-native-maps-directions';
 import tw from 'twrnc';
 import { useRef } from 'react';
-import {useSelector} from "react-redux"
+import {useSelector, useDispatch} from "react-redux"
 
 // import { useEffect } from 'react';
 
@@ -16,6 +16,7 @@ import {useSelector} from "react-redux"
 const Map = () => {
   const origin = useSelector(selectOrigin);
   const destination = useSelector(selectDestination);
+  const dispatch = useDispatch()
   const mapRef = useRef(null)
 
   useEffect(() => {
@@ -25,6 +26,19 @@ const Map = () => {
       edgePadding: {top: 50, right: 50, bottom: 50, left: 50}
     })
   }, [origin, destination])
+
+  // Calculate travel time
+  useEffect(() => {
+    if(!origin || !destination) return;
+    const getTravelTime = async() => {
+      fetch(`https://maps.googleapis.com/maps/api/distancematrix/json?destinations=${destination.description}&origins=${origin.description}&units=imperial&key=${GOOGLE_MAPS_APIKEY}`)
+      .then((res) => res.json())
+      .then(data => {
+        dispatch(setTravelTimeInformation(data.rows[0].elements[0]))
+      })
+    };
+    getTravelTime() // call function
+  }, [origin, destination, GOOGLE_MAPS_APIKEY])
   
   return (
     <MapView
