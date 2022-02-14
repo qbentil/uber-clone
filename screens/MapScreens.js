d51039ca062b797415a7cc5c32430f5975da7fc6
qbentil <Bentilshadrack72@gmail.com>
2022-02-14 05:27:58 +0000

    Creating Navigation Icons

--- screens/MapScreens.js
+++ screens/MapScreens.js
@@ -1,5 +1,5 @@
-import { StyleSheet, Text, View } from 'react-native'
-
+import { TouchableOpacity, StyleSheet, Text, View } from 'react-native'
+import { Icon } from 'react-native-elements';
 import Map from '../components/Map';
 import NavigateCard from '../components/NavigateCard';
 import React from 'react'
@@ -12,8 +12,17 @@ import { useNavigation } from '@react-navigation/native'
 
 const MapScreens = () => {
   const Stack = createStackNavigator();
+  const navigation = useNavigation()
   return (
     <View>
+      <TouchableOpacity 
+        style = {tw`bg-gray-100 absolute top-16 left-6 z-50 p-3 rounded-full shadow-lg`}
+        onPress = {() => navigation.navigate("HomeScreen")}
+      >
+          <Icon
+              name='menu'
+          />
+      </TouchableOpacity>
         <View style={tw`h-1/2`}>
             <Map />
         </View>
